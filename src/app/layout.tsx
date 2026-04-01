e336095e3207d9a1223dda16d7b6fae7efb486b4
Uday2027 <zubayerhossain1009@gmail.com>
2026-04-01 17:56:33 +0600

    Meta pixel added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
@@ -19,9 +20,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="bn" className="scroll-smooth dark">
+      <head>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '996843299576965');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
       <body
         className={`${outfit.variable} antialiased bg-background text-foreground selection:bg-blue-500/30`}
       >
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=996843299576965&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
       </body>
     </html>
